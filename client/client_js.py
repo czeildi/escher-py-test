2df4ecbe3d101ac801caaa124d5d0a1800e77b2d
Ildiko Czeller <czeildi@gmail.com>
2019-08-13 22:17:07 +0200

    create python server and client without escher

--- client/client_js.py
+++ client/client_js.py
@@ -1,11 +1,11 @@
 import requests
 from pprint import pprint
 
-port = '9292'
-path = '/validate_request_py_py'
+port = '8080'
+path = '/validate_request_py_js'
 url = 'http://localhost:' + port + path
 headers = {'X-EMS-Date': 'tomorrow'}
-response = requests.get(url, headers = headers)
+response = requests.get(url, headers=headers)
 
 print('response text body:')
 print(response.text)
@@ -14,4 +14,4 @@ print('response status:')
 print(response.status_code)
 
 print('headers:')
-pprint(dict(response.headers), width = 1)
+pprint(dict(response.headers), width=1)
--- server/server.py
+++ server/server.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route("/validate_request_js_py")
 def validate_js_py():
-    return 'Got request on validate_request_js_py'
+    print(request.headers)
+    return app.make_response(('Got request on validate_request_js_py', 200, dict(request.headers)))
 
 @app.route("/validate_request_py_py")
 def validate_py_py():
-    return 'Got request on validate_request_py_py'
+    print(request.headers)
+    return app.make_response(('Got request on validate_request_py_py', 200, dict(request.headers)))
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=9292)
