a39d33fa8e855b5e03aac2436671d9fa44a06253
Ildiko Czeller <czeildi@gmail.com>
2019-09-09 23:32:22 +0200

    attempt escher on python side

--- client/client_js.py
+++ client/client_js.py
@@ -1,11 +1,26 @@
 import requests
 from pprint import pprint
+from escherauth_go.escher_signer import EscherSigner
 
 port = '8080'
 path = '/validate_request_py_js'
-url = 'http://localhost:' + port + path
-headers = {'X-EMS-Date': 'tomorrow'}
-response = requests.get(url, headers=headers)
+host = 'http://localhost:' + port
+url = host + path
+
+def escherSignedHeader(host, path):
+    signer = EscherSigner(
+      'EscherPyExample',
+      'ildiescherpysecret',
+      'example/credential/scope'
+    )
+    signed_headers = signer.signRequest(
+        method='GET', url=path, body='', headers={'Host': host})
+    return signed_headers
+
+signed_headers = escherSignedHeader(
+  host=host, path=path#, extra_headers={'Content-Type': 'application/json'}
+)
+response = requests.get(url, headers=signed_headers)
 
 print('response text body:')
 print(response.text)
@@ -15,3 +30,4 @@ print(response.status_code)
 
 print('headers:')
 pprint(dict(response.headers), width=1)
+
